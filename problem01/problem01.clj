c3e3c0e43d9600406dd22c8fd186765ddadbd9df
Matt Hawkins <mhawkins@alextom.com>
2010-04-28 13:58:28 -0400

    problem 1 working with range.  needs to be setup to user iterate

--- problem01/problem01.clj
+++ problem01/problem01.clj
@@ -1,1 +1,9 @@
-(filter #(or (= (rem % 3) 0) (= (rem % 5) 0) ) (range 1 101))
+#!/usr/bin/env clj
+
+(defn euler1 [n]
+  (let [factor #(or (= (rem % 3) 0) (= (rem % 5) 0) )
+        factors (filter factor (range 1 (+ n 1)))]
+        (reduce + (take n factors))))
+        
+        
+(println (euler1 1000))
