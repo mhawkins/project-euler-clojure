21fe6e6a30803ec166ff26901c2b0dc2442f0023
Matt Hawkins <mhawkins@alextom.com>
2010-05-03 18:00:33 -0400

    added sum of numbers in fibonacci sequence that are even

--- problem02/problem02.clj
+++ problem02/problem02.clj
@@ -1,12 +1,14 @@
 #!/usr/bin/env clj
 
 (defn fib
+  "Fibbonacci sequence for numbers greater than 2"
   [n]
   ((fn
     [max, sequence]    
     (let [sequenceCount (count sequence) firstIndex (- sequenceCount 1) secondIndex (- sequenceCount 2) newFibNumber (+ (sequence firstIndex) (sequence secondIndex))]
     (if (> newFibNumber max)
       sequence
-      (recur max (conj sequence newFibNumber))
-    )
-  )) n [1 1] ))
+      (recur max (conj sequence newFibNumber))))) 
+      n [1 1]))
+  
+(println (reduce + (filter #(= (rem % 2) 0) (fib 4000000))))
